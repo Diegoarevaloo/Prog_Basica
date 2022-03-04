a7b9f8e5efa9859d42d711325741c5e10b939c0c
diegoarevaloo <diegoarevaloo@hotmail.com>
2022-03-04 18:17:07 -0500

    Se resuelve primer ejercicio de Freddy, aparecen pollos y gallinas en el canvas

--- villaPlatzi/platzi.js
+++ villaPlatzi/platzi.js
@@ -8,7 +8,16 @@ var fondo = {
 var vaca = {
   url: "./images/vaca.png",
   cargaOK: false
-};
+  }
+var cerdo = {
+  url: "./images/cerdo.png",
+  cargaOK: false
+  }
+var pollo = {
+  url: "./images/pollo.png",
+  cargaOK: false
+  }  ;
+
 
 var cantidad = aleatorio(1, 10);
 
@@ -20,13 +29,13 @@ vaca.imagen = new Image();
 vaca.imagen.src = vaca.url;
 vaca.imagen.addEventListener("load", cargarVacas);
 
-/*cerdo.imagen = new Image();
-cerdo.imagen.src = vaca.url;
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
 cerdo.imagen.addEventListener("load", cargarCerdos);
 
 pollo.imagen = new Image();
-pollo.imagen.src = vaca.url;
-pollo.imagen.addEventListener("load", cargarPollos);*/
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollos);
 
 function cargarFondo()
 {
@@ -39,7 +48,7 @@ function cargarVacas()
   dibujar();
 }
 
-/*function cargarCerdos()
+function cargarCerdos()
 {
   cerdo.cargaOK = true;
   dibujar();
@@ -48,7 +57,7 @@ function cargarPollos()
 {
   pollo.cargaOK = true;
   dibujar();
-}*/
+}
 
 
 
@@ -79,9 +88,59 @@ function dibujar()
         var x = x * 60;
         var y = y * 40;
         papel.drawImage(vaca.imagen, x, y);
-      }  
+      }
+    }    
+  }
+  if(cerdo.cargaOK)
+  {
+    console.log(cantidad);
+    for(var v=0; v < cantidad; v++)
+    {
+      var x = aleatorio(0, 7);
+      var y = aleatorio(0, 10);
+      var x = x * 60;
+      var y = y * 40;
+      papel.drawImage(cerdo.imagen, x, y);
+    }
+    if(cerdo.cargaOK)
+    {
+      console.log(cantidad);
+      for(var v=0; v < cantidad; v++)
+      {
+        var x = aleatorio(0, 7);
+        var y = aleatorio(0, 10);
+        var x = x * 60;
+        var y = y * 40;
+        papel.drawImage(cerdo.imagen, x, y);
+      }
+    }
+  }
+  if(pollo.cargaOK)
+  {
+    console.log(cantidad);
+    for(var v=0; v < cantidad; v++)
+    {
+      var x = aleatorio(0, 7);
+      var y = aleatorio(0, 10);
+      var x = x * 60;
+      var y = y * 40;
+      papel.drawImage(pollo.imagen, x, y);
+    }
+    if(cerdo.cargaOK)
+    {
+      console.log(cantidad);
+      for(var v=0; v < cantidad; v++)
+      {
+        var x = aleatorio(0, 7);
+        var y = aleatorio(0, 10);
+        var x = x * 60;
+        var y = y * 40;
+        papel.drawImage(pollo.imagen, x, y);
+      }
+    }        
   }
 }
+
 function aleatorio(min, maxi)
 {
   var resultado;
